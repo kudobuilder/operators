04bd6512ff3ffab86f00771c028ce1aa6e2faaf9
Ken Sipe <kensipe@gmail.com>
2020-03-26 13:24:10 -0500

    download manager
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- clean-build.sh
+++ clean-build.sh
@@ -7,8 +7,12 @@ set -o nounset
 set -o pipefail
 
 REPO_DIR=build/repo
-
+BIN_DIR=bin
 
 if [[ -d "${REPO_DIR}" ]]; then
   rm -rf $REPO_DIR
+fi
+
+if [[ -d "${BIN_DIR}" ]]; then
+  rm -rf $BIN_DIR
 fi
