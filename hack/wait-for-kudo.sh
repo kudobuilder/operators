b840d9ad24ae802ced1e867385e303def8de6798
Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>
2020-05-18 16:17:41 +0200

    waiting for kudo-manager pod phase equals `Running`
    Signed-off-by: Aleksey Dukhovniy <alex.dukhovniy@googlemail.com>

---
+++ hack/wait-for-kudo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+attempt_counter=0
+max_attempts=120
+
+while [ "$(kubectl get pod -n kudo-system kudo-controller-manager-0 -o jsonpath='{.status.phase}' || true)" != "Running" ]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      printf "\nFailed to reach KUDO manager webhook"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$((attempt_counter+1))
+    sleep 1
+done
